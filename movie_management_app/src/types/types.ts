9bc119c0bcad980c5b4dca380b5d0ceb0ab9cf9b
Tony Costa <tony@trinacode.com>
2021-11-11 11:20:43 +0100

    update types.ts

--- movie_management_app/src/types/types.ts
+++ movie_management_app/src/types/types.ts
@@ -1,7 +1,18 @@
 export interface Movie {
   id: string,
   title: string,
-  descritpion: string,
-  actors: string[],
-  notation: number
+  description: string,
+  actors: string[]
+}
+
+export interface Review {
+  id: string,
+  grade: number,
+  movie: Movie
+}
+
+export interface Actor {
+  id: string,
+  firstName: string,
+  lastName: string
 }
